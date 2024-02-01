f5710e000df2d1644f163c8c71e203df8481f71d
ojaq <abangojaq@gmail.com>
2024-02-01 14:09:44 +0700

    added bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,20 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quran</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1>Quran</h1>
-    @if (isset($quranData['data']['surahs']['references']))
-        @foreach ($quranData['data']['surahs']['references'] as $surah)
-            <h2><a href="{{ url('/surah/' . $surah['number']) }}">{{ $surah['englishName'] }}</a></h2>
-            <p>Number: {{ $surah['number'] }}</p>
-            <p>Name: {{ $surah['name'] }}</p>
-            <p>Translation: {{ $surah['englishNameTranslation'] }}</p>
-            <p>Number of Ayahs: {{ $surah['numberOfAyahs'] }}</p>
-            <p>Revelation Type: {{ $surah['revelationType'] }}</p>
-        @endforeach
-    @endif
+<body class="bg-light">
+    <div class="container mt-5">
+        <h1>Quran</h1>
+        @if (isset($quranData['data']['surahs']['references']))
+            @foreach ($quranData['data']['surahs']['references'] as $surah)
+                <div class="card my-3">
+                    <div class="card-body">
+                        <h2 class="card-title"><a href="{{ url('/surah/' . $surah['number']) }}">{{ $surah['englishName'] }}</a></h2>
+                        <p class="card-text">Number: {{ $surah['number'] }}</p>
+                        <p class="card-text">Name: {{ $surah['name'] }}</p>
+                        <p class="card-text">Translation: {{ $surah['englishNameTranslation'] }}</p>
+                        <p class="card-text">Number of Ayahs: {{ $surah['numberOfAyahs'] }}</p>
+                        <p class="card-text">Revelation Type: {{ $surah['revelationType'] }}</p>
+                    </div>
+                </div>
+            @endforeach
+        @endif
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/surah.blade.php
+++ resources/views/surah.blade.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $surahData['data']['englishName'] }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
-    <h1>{{ $surahData['data']['englishName'] }}</h1>
-    <h2>Juz: {{ $surahData['data']['ayahs'][0]['juz'] }}</h2>
 
-    @foreach ($surahData['data']['ayahs'] as $key => $ayah)
-        <h3>Ayah {{ $ayah['numberInSurah'] }}</h3>
-        <p>{{ $ayah['text'] }}</p>
-        <p>{{ $surahTranslate['data'][0]['ayahs'][$key]['text'] }}</p>
-    @endforeach
-</body
+<body>
+    <div class="container">
+        <button class="btn btn-warning mt-3"><a href="/">Home</a></button>
+        <h1>{{ $surahData['data']['englishName'] }}</h1>
+        <h2>Juz: {{ $surahData['data']['ayahs'][0]['juz'] }}</h2>
+
+        @foreach ($surahData['data']['ayahs'] as $key => $ayah)
+            <div class="card my-3">
+                <div class="card-body">
+                    <h3 class="card-title">Ayah {{ $ayah['numberInSurah'] }}</h3>
+                    <p class="card-text fs-1 text-end">{{ $ayah['text'] }}</p>
+                    <p class="card-text fs-5">{{ $surahTranslate['data'][0]['ayahs'][$key]['text'] }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
